454460430275961d2ab1837c11313b91de787aae
Michal Tomlein <michal@tomlein.org>
2022-12-06 15:52:27 +0100

    Fix Catalyst, iOS and tvOS build errors

--- Sources/Build-CIKernel/xcrun.swift
+++ Sources/Build-CIKernel/xcrun.swift
@@ -7,8 +7,23 @@
 
 import Foundation
 
+#if !os(macOS)
+@objc private protocol NSTask {
+    var executableURL: URL? { get set }
+    var arguments: [String]? { get set }
+    var standardOutput: Any? { get set }
+    var terminationStatus: Int32 { get }
+    func launch()
+    func waitUntilExit()
+}
+#endif
+
 func xcrun(_ arguments: String...) {
+    #if os(macOS)
     let process = Process()
+    #else
+    let process = unsafeBitCast((NSClassFromString("NSTask") as! NSObject.Type).init(), to: NSTask.self)
+    #endif
     process.executableURL = URL(fileURLWithPath: "/usr/bin/xcrun", isDirectory: false)
     process.arguments = arguments
     let output = Pipe()
